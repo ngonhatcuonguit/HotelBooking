c8cd64b2280ad107b3b1feb856a0c2261026daa4
CuongNgo <ngonhatcuonguit@gmail.com>
2022-08-07 01:06:27 +0700

    fix hotel_detail

--- app/src/main/java/com/cuongngo/hotel_booking/ui/hoteldetail/HotelDetailActivity.kt
+++ app/src/main/java/com/cuongngo/hotel_booking/ui/hoteldetail/HotelDetailActivity.kt
@@ -5,9 +5,48 @@ import com.cuongngo.hotel_booking.App
 import com.cuongngo.hotel_booking.R
 import com.cuongngo.hotel_booking.base.activity.BaseActivity
 import com.cuongngo.hotel_booking.databinding.ActivityHotelDetailBinding
+import com.cuongngo.hotel_booking.response.GalleryModel
+import com.cuongngo.hotel_booking.response.HotelModel
 import com.cuongngo.hotel_booking.ui.auth.LoginActivity
+import com.cuongngo.hotel_booking.ui.home.HotelAdapter
 
 class HotelDetailActivity : BaseActivity<ActivityHotelDetailBinding>() {
+
+    private lateinit var galleryAdapter: GalleryAdapter
+    var listGallery = arrayOf(
+        GalleryModel(
+            1,
+            ""
+        ),
+        GalleryModel(
+            2,
+            ""
+        ),
+        GalleryModel(
+            3,
+            ""
+        ),
+        GalleryModel(
+            4,
+            ""
+        ),
+        GalleryModel(
+            5,
+            ""
+        ),
+        GalleryModel(
+            6,
+            ""
+        ),
+        GalleryModel(
+            7,
+            ""
+        ),
+        GalleryModel(
+            8,
+            ""
+        )
+    )
 
     override fun inflateLayout() = R.layout.activity_hotel_detail
 
@@ -27,6 +66,16 @@ class HotelDetailActivity : BaseActivity<ActivityHotelDetailBinding>() {
                 startActivity(Intent(this@HotelDetailActivity, LoginActivity::class.java))
             }
         }
+        setupRcvgallery()
+    }
+
+    private fun setupRcvgallery(){
+        galleryAdapter = GalleryAdapter(
+            arrayListOf()
+        )
+
+        galleryAdapter.submitListGallery(listGallery.toList())
+        binding.rcvGallery.adapter = galleryAdapter
     }
 
     override fun setUpObserver() {
